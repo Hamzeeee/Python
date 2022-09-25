eed65990fc60e16828ca2b7f1090494df2d7b4c0
Hamzeeee <80978033+Hamzeeee@users.noreply.github.com>
2022-09-25 17:00:06 +0200

    Update Airport_Information.py

--- Airport_Information.py
+++ Airport_Information.py
@@ -2,26 +2,39 @@
 
 import requests
 
-api_key = "3cd57689-7cc9-42bb-b666-666f29152812"
-city_code = input("Insert City code of your Airport: ")
+api_key = "API-KEY"
 
+def flight_direction():
+    city_code = input("Insert City code of your Airport: ")
 
-request = requests.get(f"https://airlabs.co/api/v9/schedules?dep_iata={city_code}&api_key={api_key}").json()
+    request = requests.get(f"https://airlabs.co/api/v9/schedules?dep_iata={city_code}&api_key={api_key}").json()
 
-flights = []
+    lenght = request['response']
 
-lenght = request['response']
+    count = input(f"Amount of Airplanes you want to see (limit is {len(lenght)}): ")
 
-count = input(f"Amount of Airplanes you want to see (limit is {len(lenght)}): ")
+    print("[  Status  ]    [  Airline  ]    [  Depart  ]    [  Landing  ]          [  Started  ]                              [  Landed  ]                  [  Flight ID  ]")
+    if count == "all":
+        count = len(lenght)
 
-
-print("[ Status ]                 [ Airline ]         [ Depart ]         [ Landing ]                 [ Started ]                  [ Landed ]             [ Flight ID ]")
-for x in range(int(count)):
-    airline = request['response'][x]['airline_icao']
-    status = request['response'][x]['status']
-    depart = request['response'][x]['dep_iata']
-    arrive = request['response'][x]['arr_iata']
-    landed = request['response'][x]['arr_time']
-    started = request['response'][x]['dep_time']
-    flight_id = request['response'][x]['flight_iata']
-    print(f"  {status}                       {airline}                 {depart}                {arrive}                  {started}            {landed}              {flight_id}")
+    for x in range(int(count)):
+        airline = request['response'][x]['airline_icao']
+        status = request['response'][x]['status']
+        depart = request['response'][x]['dep_iata']
+        arrive = request['response'][x]['arr_iata']
+        landed = request['response'][x]['arr_time']
+        started = request['response'][x]['dep_time']
+        flight_id = request['response'][x]['flight_iata']
+        
+        # Formatting Text
+        if(status == "cancelled"):
+            text = f" {status}           {airline}             {depart}              {arrive}              {started}                          {landed}                     {flight_id}"
+            print(text)
+        if(status == "landed"):
+            text = f" {status}              {airline}             {depart}              {arrive}              {started}                          {landed}                     {flight_id}"
+            print(text)
+        if(status == "redirected"):
+            text = f" {status}          {airline}             {depart}              {arrive}              {started}                          {landed}                     {flight_id}"
+            print(text)
+        
+flight_direction()
